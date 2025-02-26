60f5bbcf12d9bddb8bea412f438fe5128fa79e8e
Loup Thevenin <loup@MacBook-Pro-de-Loup.local>
2025-02-26 08:12:07 +0100

    delete print_config

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: opdi-bia <opdi-bia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:04:05 by ltheveni          #+#    #+#             */
-/*   Updated: 2025/02/13 15:55:56 by ltheveni         ###   ########.fr       */
+/*   Updated: 2025/02/25 15:38:16 by ltheveni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,35 +19,6 @@ static void	error_main(char *s)
 	exit(EXIT_FAILURE);
 }
 
-static void	print_config(t_config config)
-{
-	int	i;
-
-	printf("Textures:\n");
-	printf("  - North: %s\n", config.texture_n);
-	printf("  - South: %s\n", config.texture_s);
-	printf("  - East:  %s\n", config.texture_e);
-	printf("  - West:  %s\n", config.texture_w);
-	printf("\nColors:\n");
-	printf("  - Floor:   RGB(%d, %d, %d)\n", config.floor_color.r,
-		config.floor_color.g, config.floor_color.b);
-	printf("  - Ceiling: RGB(%d, %d, %d)\n", config.ceiling_color.r,
-		config.ceiling_color.g, config.ceiling_color.b);
-	printf("\nMap Size: Width = %d, Height = %d\n", config.map_width,
-		config.map_height);
-	printf("\nPlayer Position:\n");
-	printf("  - X: %d\n", config.player_x);
-	printf("  - Y: %d\n", config.player_y);
-	printf("  - Direction: %c\n", config.player_dir);
-	printf("\nMap:\n");
-	i = 0;
-	while (config.map[i])
-	{
-		printf("  %s\n", config.map[i]);
-		i++;
-	}
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	t_config	config;
@@ -57,7 +28,6 @@ int	main(int argc, char **argv, char **envp)
 	if (argc != 2)
 		error_main("./cub3D *.cub\n");
 	init_config(&config, argv[1]);
-	print_config(config);
 	set_window(&config);
 	free_config(&config);
 	return (0);
